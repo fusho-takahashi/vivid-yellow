9590caeb44bee4cb6c70ee2facbe749353df9e5a
fusho-takahashi <zenryaku@fusho-takahashi.me>
2025-12-30 09:45:46 +0900

    サイトURLを更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vivid-yellow.pages.dev',
+  site: 'https://blog.fusho-takahashi.me',
   output: 'static',
   build: {
     assets: 'assets'
